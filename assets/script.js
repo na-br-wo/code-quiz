4efc22e93ba783a59506b28b49e2e06a71dafbd1
na-br-wo <nathan.b.wood@gmail.com>
2023-03-22 00:05:44 -0600

    set up html skeleton, added an empty array to script.js for the questions

--- assets/script.js
+++ assets/script.js
@@ -1,53 +1,103 @@
 // Set of quiz questions
 const questions = [
   {
-    question: "",
-    answers: [""],
-    correctAnswer:""
+    question: 'Which HTML element is used to embed javascript?',
+    answers: {
+      a: '<js>',
+      b: '<javascript>',
+      c: '<jscode>',
+      d: '<script>'
+    },
+    correctAnswer: 'd'
   },
   {
-    question: "",
-    answers: [""],
-    correctAnswer:""
+    question: 'Where can Javascript be inserted in an HTML file?',
+    answers: {
+      a: 'Both the <head> and <body> sections',
+      b: 'Only the <head> section',
+      c: 'Only the <body> section',
+      d: 'Only in an external file'
+    },
+    correctAnswer: 'a'
   },
   {
-    question: "",
-    answers: [""],
-    correctAnswer:""
+    question: 'What is the correct syntax when using an external script named "script.js"?',
+    answers: {
+      a: '<script href="script.js">',
+      b: '<script>script.js</script>',
+      c: '<script src="script.js">',
+      d: 'script == script.js'
+    },
+    correctAnswer: 'c'
   },
   {
-    question: "",
-    answers: [""],
-    correctAnswer:""
+    question: 'Which of the following lines of code will successfully create a function in Javascript?',
+    answers: {
+      a: 'function = myFunction()',
+      b: 'myFunction = function',
+      c: 'function myFunction()',
+      d: 'function:myFunction()'
+    },
+    correctAnswer: 'c'
   },
   {
-    question: "",
-    answers: [""],
-    correctAnswer:""
+    question: 'In Javascript syntax, how do you call a function named "funcA"?',
+    answers: {
+      a: 'funcA();',
+      b: 'call funcA();',
+      c: 'funcA(call);',
+      d: 'functionCall funcA();'
+    },
+    correctAnswer: 'a'
   },
   {
-    question: "",
-    answers: [""],
-    correctAnswer:""
+    question: 'Which of the following is the correct syntax for writing an IF statement in Javascript?',
+    answers: {
+      a: 'if i = 10',
+      b: 'if (i == 10)',
+      c: 'if (i == 10), then',
+      d: 'if i = 10, then'
+    },
+    correctAnswer: 'b'
   },
   {
-    question: "",
-    answers: [""],
-    correctAnswer:""
+    question: 'How would you write an IF statement that will execute if "i" is NOT equal to 10?',
+    answers: {
+      a: 'if i =! 5 then',
+      b: 'if i <> 5',
+      c: 'if (i <> 5)',
+      d: 'if (i != 5)'
+    },
+    correctAnswer: 'd'
   },
   {
-    question: "",
-    answers: [""],
-    correctAnswer:""
+    question: 'How do you start a WHILE loop in Javascript?',
+    answers: {
+      a: 'while {i}',
+      b: 'while (i <=10)',
+      c: 'while (i <=10; i++)',
+      d: 'while [i = 1, i = 10]'
+    },
+    correctAnswer: 'b'
   },
   {
-    question: "",
-    answers: [""],
-    correctAnswer:""
+    question: 'How do you initialize a Javascript array?',
+    answers: {
+      a: 'const array = ["one", "two", "three"]',
+      b: 'const array = []("one", "two", "three")',
+      c: 'const array = "one", "two", "three"',
+      d: 'array(["one", "two", "three"])'
+    },
+    correctAnswer: 'a'
   },
   {
-    question: "",
-    answers: [""],
-    correctAnswer:""
+    question: 'What event occurs when the user clicks on an HTML element?',
+    answers: {
+      a: 'onMouseClick',
+      b: 'onMouseButton',
+      c: 'onclick',
+      d: 'buttonClick'
+    },
+    correctAnswer: 'c'
   }
 ]
