33d3942c23f83e11afb039d12e43d1e37f59bd1d
na-br-wo <nathan.b.wood@gmail.com>
2023-03-21 23:32:23 -0600

    set up html skeleton, added an empty array to script.js for the questions

---
+++ assets/script.js
@@ -0,0 +1,53 @@
+// Set of quiz questions
+const questions = [
+  {
+    question: "",
+    answers: [""],
+    correctAnswer:""
+  },
+  {
+    question: "",
+    answers: [""],
+    correctAnswer:""
+  },
+  {
+    question: "",
+    answers: [""],
+    correctAnswer:""
+  },
+  {
+    question: "",
+    answers: [""],
+    correctAnswer:""
+  },
+  {
+    question: "",
+    answers: [""],
+    correctAnswer:""
+  },
+  {
+    question: "",
+    answers: [""],
+    correctAnswer:""
+  },
+  {
+    question: "",
+    answers: [""],
+    correctAnswer:""
+  },
+  {
+    question: "",
+    answers: [""],
+    correctAnswer:""
+  },
+  {
+    question: "",
+    answers: [""],
+    correctAnswer:""
+  },
+  {
+    question: "",
+    answers: [""],
+    correctAnswer:""
+  }
+]
